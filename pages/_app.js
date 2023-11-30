0599fadf4e1a20b0c1a3c516eda310f35301e9f3
Vedant Jain <jscafe.business@gmail.com>
2023-11-30 09:42:09 +0530

    Setup project and socket.io

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { SocketProvider } from "@/context/socket";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SocketProvider>
+      <Component {...pageProps} />
+    </SocketProvider>
+  );
 }
---
+++ pages/api/socket.js
@@ -0,0 +1,20 @@
+import { Server } from "socket.io";
+
+const SocketHandler = (req, res) => {
+    console.log("called api")
+    if (res.socket.server.io) {
+        console.log("socket already running")
+    } else {
+        const io = new Server(res.socket.server)
+        res.socket.server.io = io
+    
+        io.on('connection', (socket) => {
+            console.log("server is connected")
+        })
+    }
+    res.end();
+}
+
+
+export default SocketHandler;
+
